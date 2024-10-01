4fd153f785b03a417b5236f8878a69de3df22e37
George James <georgegyang3@gmail.com>
2024-10-01 07:55:27 +0100

    fixes

--- src/components/flexible-courses/CourseCard.jsx
+++ src/components/flexible-courses/CourseCard.jsx
@@ -2,7 +2,10 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import image from "../../assets/jpg/course-img.jpg"
 import { MdGroups } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 function CourseCard() {
+    const navigate = useNavigate();
+
     return (
         <Card className='w-100'>
             <Card.Img variant="top" src={image} />
@@ -14,12 +17,12 @@ function CourseCard() {
                 <p className="fs-5 fw-semibold">£195</p>
 
                 <div className="d-flex">
-                    <button className='btn mt-4 me-3 secondary_bg hover_effect text-white custom_btn border-2 p-2 fw-semibold f-5' >View Course</button>
-                    <button className='btn mt-4 me-3 primary_bg hover_effect text-white custom_btn border-2 p-2 fw-semibold f-5' >Book Now</button>
+                    <button onClick={()=> navigate("/course/id")} className='btn mt-4 me-3 secondary_bg hover:bg-sky-600 hover_effect text-white custom_btn border-2 p-2 fw-semibold f-5' >View Course</button>
+                    <button className='btn mt-4 me-3 primary_bg hover_effect hover:bg-purple-600 text-white custom_btn border-2 p-2 fw-semibold f-5' >Book Now</button>
                 </div>
                 <div className="d-flex mt-3">
                     <span className="me-1 primary_color"><MdGroups size={20} /></span>
-                    <span className="primary_color me-1">1000+ </span> <span style={{color: "#c1c2c9"}}>have completed this course</span>
+                    <span className="primary_color me-1">1000+ </span> <span style={{ color: "#c1c2c9" }}>have completed this course</span>
                 </div>
             </Card.Body>
         </Card>
